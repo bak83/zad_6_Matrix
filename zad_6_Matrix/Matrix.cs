3aee238ed8e1393228751ba88f017d49536ab107
bak83 <bak.dariusz@gmail.com>
2020-01-31 19:42:59 +0100

    częsciowo zrobione

--- zad_6_Matrix/Matrix.cs
+++ zad_6_Matrix/Matrix.cs
@@ -6,34 +6,61 @@ namespace zad_6_Matrix
 	{
 		public int M, N;
 		public double[,] matrix;
+		public double[,] matrix2;
 		public Matrix(int m, int n)
 		{
 			matrix = new double[m, n];
 			M = m;
 			N = n;
 		}
-		public void insertData(double [,]matrix)
+		public void insertData(double[,] matrix)
 		{
 			Console.WriteLine("podaj liczbe");
 
-			for (int i = 0;i<M;i++)
+			for (int i = 0; i < M; i++)
 			{
-				for (int j=0;j<N;j++)
+				for (int j = 0; j < N; j++)
 				{
 					matrix[i, j] = double.Parse(Console.ReadLine());
 				}
 			}
 		}
-		public double AddMatrix(double[,] m1, double[,] m2)
+		public double[,] AddMatrix(double[,] m1, double[,] m2)
 		{
 			for (int i = 0; i < M; i++)
 			{
 				for (int j = 0; j < N; j++)
 					matrix[i, j] = m1[i, j] + m2[i, j];
-
 			}
 
-			return 0;
+			return matrix;
+		}
+
+		public double[,] MultipleMatrix(double[,] m1, double[,] m2)
+		{
+			int RowsM1 = m1.GetLength(0);
+			int ColumnM1 = m1.GetLength(1);
+			int RowsM2 = m1.GetLength(0);
+			int ColumnM2 = m1.GetLength(1);
+			double[,] m3 = new double[RowsM1, ColumnM2];
+			if (ColumnM1 != RowsM2)
+			{
+				Console.WriteLine("Niewykonalne");
+			}
+			else
+			{
+				for (int i = 0; i < RowsM1; i++)
+				{
+					for (int j = 0; i < ColumnM2; j++)
+					{
+						for (int k = 0; k < ColumnM1; k++)
+						{
+
+						}
+					}
+				}
+			}
+			return m3;
 		}
 	}
 }
--- zad_6_Matrix/Program.cs
+++ zad_6_Matrix/Program.cs
@@ -15,11 +15,12 @@ namespace zad_6_Matrix
 			Console.WriteLine("Maxierz pierwsza: ");
 			m1.insertData(m2.matrix);
 
+			Matrix m3 = new Matrix(2, 3);
+			m3.matrix = m3.AddMatrix(m1.matrix, m2.matrix);
 
-
-			Console.WriteLine(M.GetLength(0));
-			Console.WriteLine(M.GetLength(1));
-			Console.WriteLine(m1.matrix[0, 0]);
+			//Console.WriteLine(M.GetLength(0));
+			//Console.WriteLine(M.GetLength(1));
+			Console.WriteLine(m3.matrix[0, 0]);
 
 			Console.WriteLine("dupa");
 			Console.ReadKey();
