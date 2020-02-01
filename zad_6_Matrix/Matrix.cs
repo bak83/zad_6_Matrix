f547ed5921454ba877a1b05e5462df800b42edeb
bak83 <bak.dariusz@gmail.com>
2020-02-01 11:00:42 +0100

    commit

--- zad_6_Matrix/Matrix.cs
+++ zad_6_Matrix/Matrix.cs
@@ -7,13 +7,21 @@ namespace zad_6_Matrix
 		public int M, N;
 		public double[,] matrix;
 		public double[,] matrix2;
+		public int Rows
+		{
+			get { return matrix.GetLength(0); }
+		}
+		public int Column
+		{
+			get { return matrix.GetLength(1); }
+		}
 		public Matrix(int m, int n)
 		{
 			matrix = new double[m, n];
 			M = m;
 			N = n;
 		}
-		public void insertData(double[,] matrix)
+		public void insertData(Matrix m)
 		{
 			Console.WriteLine("podaj liczbe");
 
@@ -21,46 +29,92 @@ namespace zad_6_Matrix
 			{
 				for (int j = 0; j < N; j++)
 				{
-					matrix[i, j] = double.Parse(Console.ReadLine());
+					m.matrix[i, j] = double.Parse(Console.ReadLine());
 				}
 			}
 		}
-		public double[,] AddMatrix(double[,] m1, double[,] m2)
+		public Matrix AddMatrix(Matrix m1, Matrix m2)
 		{
-			for (int i = 0; i < M; i++)
-			{
-				for (int j = 0; j < N; j++)
-					matrix[i, j] = m1[i, j] + m2[i, j];
-			}
+			Matrix m3 = new Matrix(m1.matrix.GetLength(0), m2.matrix.GetLength(1));
 
-			return matrix;
+			if ((m1.matrix.GetLength(0) != m2.matrix.GetLength(0)) | (m1.matrix.GetLength(1) != m2.matrix.GetLength(1)))
+			{
+				Console.WriteLine("Dodawnie niewykonalne");
+			}
+			else
+			{
+				for (int i = 0; i < M; i++)
+				{
+					for (int j = 0; j < N; j++)
+						m3.matrix[i, j] = m1.matrix[i, j] + m2.matrix[i, j];
+				}
+			}
+			return m3;
 		}
 
-		public double[,] MultipleMatrix(double[,] m1, double[,] m2)
+		public double[,] MultipleMatrix(Matrix m1, Matrix m2)
 		{
-			int RowsM1 = m1.GetLength(0);
-			int ColumnM1 = m1.GetLength(1);
-			int RowsM2 = m1.GetLength(0);
-			int ColumnM2 = m1.GetLength(1);
+			int RowsM1 = m1.matrix.GetLength(0);
+			int ColumnM1 = m1.matrix.GetLength(1);
+			int RowsM2 = m2.matrix.GetLength(0);
+			int ColumnM2 = m2.matrix.GetLength(1);
+			double tmp;
 			double[,] m3 = new double[RowsM1, ColumnM2];
 			if (ColumnM1 != RowsM2)
 			{
-				Console.WriteLine("Niewykonalne");
+				Console.WriteLine("Mnożenie niewykonalne");
 			}
 			else
 			{
 				for (int i = 0; i < RowsM1; i++)
 				{
-					for (int j = 0; i < ColumnM2; j++)
+					for (int j = 0; j < ColumnM2; j++)
 					{
+						tmp = 0;
 						for (int k = 0; k < ColumnM1; k++)
 						{
-
+							tmp += m1.matrix[i, k] * m2.matrix[k, j];
 						}
+						m3[i, j] = tmp;
 					}
 				}
 			}
 			return m3;
 		}
+		public int RowsCounter(double[,] m1)
+		{
+			return m1.GetLength(0);
+		}
+		public int ColumnCounter(double[,] m1)
+		{
+			return m1.GetLength(1);
+		}
+
+		public string ShowMatrix()
+		{
+			string s = "";
+			for (int x = 0; x < Rows; x++)
+			{
+				for (int y = 0; y < Column; y++)
+				{
+					s += String.Format("{0}\t", matrix[x, y]);
+				}
+				s += '\n';
+			}
+			return s;
+		}
+		public Matrix ConstMultipli(Matrix m1, double x)
+		{
+			Matrix m2 = new Matrix(m1.Rows, m1.Column);
+			for (int i = 0; i < m1.Rows; i++)
+			{
+				for (int j = 0; j < m1.Column; j++)
+				{
+					m2.matrix[i, j] = m1.matrix[i, j] * x;
+				}
+			}
+			return m2;
+		}
+
 	}
 }
--- zad_6_Matrix/Program.cs
+++ zad_6_Matrix/Program.cs
@@ -6,18 +6,40 @@ namespace zad_6_Matrix
 	{
 		static void Main(string[] args)
 		{
-			double[,] M = new double[2,3];
-			Matrix m1 = new Matrix(2,3);
-			Console.WriteLine("Maxierz pierwsza: ");
-			m1.insertData(m1.matrix);
+			//double[,] M = new double[2,3];
+			var m1 = new Matrix(2, 2);
+			Console.WriteLine("Macierz pierwsza: ");
+			m1.insertData(m1);
+			Console.WriteLine("Twoja macierz:");
+			Console.WriteLine(m1.ShowMatrix());
 
-			Matrix m2 = new Matrix(2, 3);
-			Console.WriteLine("Maxierz pierwsza: ");
-			m1.insertData(m2.matrix);
+			var m2 = new Matrix(2,2);
+			Console.WriteLine("Macierz druga: ");
+			m2.insertData(m2);
+			Console.WriteLine("Twoja macierz:");
+			Console.WriteLine(m2.ShowMatrix());
 
-			Matrix m3 = new Matrix(2, 3);
-			m3.matrix = m3.AddMatrix(m1.matrix, m2.matrix);
+			//Matrix m3 = new Matrix(2, 2);
+			//m3.matrix = m3.AddMatrix(m1, m2);
+			//Console.WriteLine("Wynik dodawania, twoja macierz:");
+			//Console.WriteLine(m3.ShowMatrix());
+			var m3 = m1.AddMatrix(m1, m2);
+			Console.WriteLine("Wynik dodawania, twoja macierz:");
+			Console.WriteLine(m3.ShowMatrix());
 
+			//var a = m1.ColumnCounter(m1.matrix);
+			////var b = m2.RowsCounter(m2.matrix);
+			Matrix m4 = new Matrix(m1.ColumnCounter(m1.matrix), m2.RowsCounter(m2.matrix));
+			m4.matrix = m4.MultipleMatrix(m1, m2);
+			Console.WriteLine("Wynik mnożenia, twoja macierz:");
+			Console.WriteLine(m4.ShowMatrix());
+
+			var m5 = m1.ConstMultipli(m1, 2.5);
+			Console.WriteLine("Mnożenie przez stałą:");
+			Console.WriteLine(m5.ShowMatrix());
+
+			//Console.WriteLine(m4.matrix[0, 0]);
+			//Console.WriteLine(m4.matrix.GetLength(0).ToString(), m4.matrix.GetLength(1).ToString());
 			//Console.WriteLine(M.GetLength(0));
 			//Console.WriteLine(M.GetLength(1));
 			Console.WriteLine(m3.matrix[0, 0]);
